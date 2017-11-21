f78328fce69ac1f1147b9f6ab399aeb405d72e9b
Negin <negin.namiranian@gmail.com>
2017-11-20 19:16:17 -0800

    Server and routers been made

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+
+
+var friendsArray=[
+	{
+		"name": "Elmo",
+		"photo": "...",
+		"scores":[
+			2,
+			2,
+			1,
+			3,
+			4,
+			5,
+			5,
+			1,
+			4,
+			1
+		]
+
+	},
+	{
+		"name": "Cookie Monster",
+		"photo": "...",
+		"scores":[
+			1,
+			3,
+			1,
+			4,
+			2,
+			1,
+			1,
+			5,
+			2,
+			1
+		]
+
+	},
+	{
+		"name": "Oscar the Grouch",
+		"photo": "...",
+		"scores":[
+			1,
+			1,
+			1,
+			5,
+			3,
+			4,
+			1,
+			5,
+			2,
+			3
+		]
+
+	},
+	{
+		"name": "Kermit the Frog",
+		"photo": "...",
+		"scores":[
+			5,
+			1,
+			4,
+			5,
+			5,
+			2,
+			2,
+			5,
+			4,
+			3
+		]
+
+	},
+	{
+		"name": "Abby Cadabby",
+		"photo": "...",
+		"scores":[
+			2,
+			2,
+			1,
+			1,
+			5,
+			5,
+			5,
+			3,
+			2,
+			4
+		]
+
+	},
+	{
+		"name": "Big Bird",
+		"photo": "...",
+		"scores":[
+			3,
+			2,
+			4,
+			5,
+			3,
+			3,
+			1,
+			2,
+			1,
+			1
+		]
+
+	},
+];
+
+module.exports = friendsArray;
