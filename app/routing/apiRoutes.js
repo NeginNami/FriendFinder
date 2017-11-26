b371e0d8674782703ef07ac6315b94aef7118cee
Negin <negin.namiranian@gmail.com>
2017-11-25 17:00:09 -0800

    Logic added to pick up the most similar character

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -22,6 +22,40 @@ module.exports = function(app) {
   	console.log(req.body.scores);
   	console.log(req.body.scores[9]);
   	//console.log(friendsData);
+  	//-------------------------------------logic-------------------------
+
+  	var diff=[];
+  	
+
+  	for (var i = 0; i < friendsData.length; i++) {
+  		console.log("======================================");
+  		diff[i]=0;
+  		for (var j = 0; j<10; j++) {
+  			//console.log(counter+": "+(parseInt(friendsData[i].scores[j])+10));
+
+  			diff[i]= diff[i]+ Math.abs(parseInt(req.body.scores[j])-parseInt(friendsData[i].scores[j]));
+  			console.log(Math.abs(parseInt(req.body.scores[j])-parseInt(friendsData[i].scores[j])));
+  		}
+  	}
+  	console.log(diff);
+
+  	var lessDiffIndex=0;
+  	var minDiff=diff[0];
+  	for (var i = 1; i < diff.length; i++) {
+  		if (diff[i]<minDiff) {
+  			lessDiffIndex=i;
+  			minDiff=diff[i];
+  		}
+  	}
+  	console.log("Less dif index: "+lessDiffIndex);
+  	console.log(friendsData[lessDiffIndex]);
+  	console.log(friendsData[lessDiffIndex].name);
+
+
+
+
+
+  	//-------------------------------------Last Part------------------------
   	friendsData.push(req.body);
   	console.log(friendsData);
   	res.json(true);
